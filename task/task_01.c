e6046400d2521d6561d43f657756fa8dc8358158
lxlenovostar <lxlenovostar@gmail.com>
2014-04-09 17:59:04 +0800

    update file

--- task/task_01.c
+++ task/task_01.c
@@ -1,14 +1,15 @@
 #include <linux/module.h>               // for printk()
+#include <linux/kernel.h>               // for KERN_DEBUG  
 
 static int  __init init_page_dir( void )
 {
-	printk("Hello world\n");
+	printk(KERN_DEBUG "Hello world\n");
 	return 0;
 }
 
 static void __exit exit_page_dir( void )
 {
-	printk("\n   Goodbye now... \n\n" );
+	printk(KERN_DEBUG "\n   Goodbye now... \n\n" );
 }
 
 MODULE_LICENSE("GPL");
