f443715a360a95e3a2a724dee3facc52dae3e3cf
lxlenovostar <lxlenovostar@gmail.com>
2014-09-09 00:15:21 +0800

    可运行版本

--- code/mmap/my_mmap.c
+++ code/mmap/my_mmap.c
@@ -27,6 +27,23 @@
 #include <linux/version.h>
 #include <net/ip.h>
 #include <net/checksum.h>
+#include <linux/inetdevice.h>
+#include <linux/netdevice.h>
+#include <net/ip.h>
+#include <net/tcp.h>
+#include <net/udp.h>
+#include <net/icmp.h>
+#include <net/arp.h>
+#include <linux/if_arp.h>
+#include <linux/if_ether.h>
+#include <net/route.h>
+#include <net/neighbour.h>
+#include <net/netevent.h>
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32))
+#include <net/net_namespace.h>
+#endif
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
 #include <linux/uaccess.h>
 #include <linux/timer.h>
 #include <linux/pid.h>
@@ -45,6 +62,7 @@
         ((unsigned char *)&addr)[3]
 #endif
 
+#define DST_MAC {0x00, 0x16, 0x31, 0xf0, 0x9d, 0xc4}
 static int MAJOR_DEVICE = 30;
 void * mmap_buf = 0;
 unsigned long mmap_size = 4*1024;
@@ -183,34 +201,6 @@ static const struct file_operations ws_fops ={
 		//.release = ws_release,
 };
 
-#define WORD 32  
-#define SHIFT 5 ////移动5个位,左移则相当于乘以32,右移相当于除以32取整  
-#define MASK 0x1F //16进制下的31  
-#define N 10000000  
-int bitmap[1 + N / WORD];  
-int *bitmap_addr;
-
-/* 
- * 要进行置1位操作，必须先确定逻辑位置：
- * 字节位置(数组下标)和位位置。
- * 字节位置=数据/32;(采用位运算即右移5位)
- * 位位置=数据%32;(采用位运算即跟0X1F进行与操作)。
- * m mod n 运算，当n = 2的X次幂的时候,m mod n = m&(n-1) 
- */  
-void set(int i) {  
-	bitmap[i >> SHIFT] |= (1 << (i & MASK));  
-}  
-
-/* 清除位操作，用&~操作符 */  
-void clear(int i) {  
-    bitmap[i >> SHIFT] &= ~(1 << (i & MASK));  
-}  
-
-/* 测试位操作用&操作符 */  
-int test(int i) {  
-    return bitmap[i >> SHIFT] & (1 << (i & MASK));  
-}  
-
 RingBuffer *RingBuffer_create(void *start_malloc, int length)
 {
 		RingBuffer *buffer = (RingBuffer *)start_malloc;
@@ -266,6 +256,7 @@ unsigned int hook_local_in_p(unsigned int hooknum, struct sk_buff **pskb, const 
 }
 #endif
 
+
 unsigned int hook_local_in(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out,int (*okfn)(struct sk_buff *))
 {
 		/*
@@ -278,7 +269,6 @@ unsigned int hook_local_in(unsigned int hooknum, struct sk_buff *skb, const stru
 		struct udphdr *udph = NULL;
 		struct iphdr *send_iph = NULL;
 		struct ethhdr *eth = NULL;
-		struct net_device *dev = skb->dev;
 
 		struct tcphdr *th = NULL;
 		__be32 saddr, daddr;
@@ -313,50 +303,40 @@ unsigned int hook_local_in(unsigned int hooknum, struct sk_buff *skb, const stru
 				struct timeval tv;
 				do_gettimeofday(&tv);
 				ts_begin = tv.tv_sec;
-				printk("second begin is %ld\n", ts_begin);
+				//printk("second begin is %ld\n", ts_begin);
 			}
 		atomic_inc(&packet_count);
 
-		printk("what0 port is %d\n", ntohs(dport));	
 		if (ntohs(dport) == 80) {
-			printk("get http packet num is %d\n", drop_count);	
+			//printk("get http packet num is %d\n", drop_count);	
 			/*
 			 * write into the ring buffer
 			 */
-			//memcpy(fix_buffer, skb->data, ulen);
-			//memset(fix_buffer + ulen, '\0', sizeof(fix_buffer) - ulen);
-			//printk("fix_buffer %d\n", strlen(fix_buffer));
 			//if (RingBuffer_write(ring_buffer, skb->data, ulen) == ulen) {
 			
-			//memcpy(ring_buffer + SLOT, skb->data, ulen);
-			//printk("Packet %d.%d.%d.%d:%d -> %d.%d.%d.%d:%d ulen=%d\n", \
-				NIPQUAD(in_aton(dest_addr)), ntohs(sport), NIPQUAD(daddr), ntohs(dport), ulen);
+			memcpy(mmap_buf, skb->data, ulen);
 			atomic_inc(&drop_count);
 
 			/*
 			* send packet 
 			*/
-			char *dest_addr = "192.168.109.176";
+			char *dest_addr = "192.168.99.1";
 			int eth_len, udph_len, iph_len, len;
 			eth_len = sizeof(struct ethhdr);
 			iph_len = sizeof(struct iphdr);
 			udph_len  = sizeof(struct udphdr);
 			len	= eth_len + iph_len + udph_len;
-
 			struct sk_buff *send_skb = alloc_skb(len, GFP_ATOMIC);
 			if (!send_skb)
 				return NF_DROP;
 
-			//skb_put(send_skb, len);
 			skb_reserve(send_skb, len);
-			 
 			skb_push(send_skb, sizeof(struct udphdr));
-			 
 			skb_reset_transport_header(send_skb);
 			
 			udph = udp_hdr(send_skb);
 			udph->source = dport;
-			udph->dest = dport;
+			udph->dest = htons(ntohs(dport) + 1);
 			udph->len = htons(udph_len);
 			udph->check = 0;
 			udph->check = csum_tcpudp_magic(daddr, in_aton(dest_addr), udph_len, IPPROTO_UDP, csum_partial(udph, udph_len, 0));
@@ -371,7 +351,7 @@ unsigned int hook_local_in(unsigned int hooknum, struct sk_buff *skb, const stru
 			// iph->version = 4; iph->ihl = 5; 
 			put_unaligned(0x45, (unsigned char *)send_iph);
 			send_iph->tos      = 0;
-			put_unaligned(htons(iph_len), &(send_iph->tot_len));
+			put_unaligned(htons(iph_len) + htons(udph_len), &(send_iph->tot_len));
 			//send_iph->id       = htons(atomic_inc_return(&ip_ident));
 			send_iph->id       = 0;
 			send_iph->frag_off = 0;
@@ -382,11 +362,13 @@ unsigned int hook_local_in(unsigned int hooknum, struct sk_buff *skb, const stru
 			put_unaligned(in_aton(dest_addr), &(send_iph->daddr));
 			send_iph->check    = ip_fast_csum((unsigned char *)send_iph, send_iph->ihl);
 			  
+			struct net_device *dev = skb->dev;
 			eth = (struct ethhdr *) skb_push(send_skb, ETH_HLEN);
 			skb_reset_mac_header(send_skb);
 			send_skb->protocol = eth->h_proto = htons(ETH_P_IP);
 			memcpy(eth->h_source, dev->dev_addr, ETH_ALEN);
-			memcpy(eth->h_dest, "00:0C:29:DC:2D:F5", ETH_ALEN);
+			u8 dst_mac[ETH_ALEN] = DST_MAC;
+			memcpy(eth->h_dest, dst_mac, ETH_ALEN);
 
 			send_skb->dev = dev;
 			dev_queue_xmit(send_skb);
