e00301a7c05b752441b5e5387c9b5bd7bc1a6c82
lxlenovostar <lxlenovostar@gmail.com>
2014-11-24 07:17:29 +0800

    pass

--- usaco/transform.c
+++ usaco/transform.c
@@ -14,210 +14,160 @@ char test[10][10];
 /*
  * 反射
  **/
-void transform_reflect(char (*t_src)[10])
+void
+transform_reflect(char (*t_src)[10])
 {
-		int i, j, k;
-		char tmp[10][10];
-		
-		for (i = 0; i < num; ++i){
-			strncpy((char*)(tmp[i]), (const char *)(t_src + i), num);
-		}	
+	int i, j, k;
+	char tmp[10][10];
 
-		for (i = 0; i < num; ++i){
-				k = num - 1;
-				for (j = 0; j < num; ++j){
-						test[i][j] = tmp[i][k];
-						--k;
-				}
+	for (i = 0; i < num; ++i) {
+		strncpy((char *) (tmp[i]), (const char *) (t_src + i), num);
+	}
+
+	for (i = 0; i < num; ++i) {
+		k = num - 1;
+		for (j = 0; j < num; ++j) {
+			test[i][j] = tmp[i][k];
+			--k;
 		}
-
+	}
 }
 
 /*
  * 返回选择90度的图形
  **/
-void transform_90(char (*t_src)[10])
+void
+transform_90(char (*t_src)[10])
 {
-		int i, j, k;
-		char tmp[10][10];
-		
-		for (i = 0; i < num; ++i){
-			strncpy((char*)(tmp[i]), (const char *)(t_src + i), num);
-		}	
+	int i, j, k;
+	char tmp[10][10];
 
-		/*	
-		for (i = 0; i < num; ++i)
-				for (j = 0; j < num; ++j)
-				{
-						printf("%c", tmp[i][j]);
+	for (i = 0; i < num; ++i) {
+		strncpy((char *) (tmp[i]), (const char *) (t_src + i), num);
+	}
 
-						if (j == num - 1)
-								printf("\n");
-				}
-		*/
-
-		for (i = 0; i < num; ++i){
-				k = num - 1;
-				for (j = 0; j < num; ++j){
-					test[i][j] = tmp[k][i];
-					--k;
-				}
+	for (i = 0; i < num; ++i) {
+		k = num - 1;
+		for (j = 0; j < num; ++j) {
+			test[i][j] = tmp[k][i];
+			--k;
 		}
-		
-		/*
-		printf("end\n");
-		for (i = 0; i < num; ++i)
-				for (j = 0; j < num; ++j)
-				{
-						printf("%c", test[i][j]);
-
-						if (j == num - 1)
-								printf("\n");
-				}
-		*/
+	}
 }
 
 /*
  * return 1 strand for not equal.
  * **/
-int cmp(char (*cmp)[10], int num)
+int
+cmp(char (*cmp)[10], int num)
 {
-		int i;
-		for (i = 0; i < num; ++i){
-			if (strcmp(cmp[i], test[i]) != 0){
-					return 1;
-			}	
+	int i;
+	for (i = 0; i < num; ++i) {
+		if (strcmp(cmp[i], test[i]) != 0) {
+			return 1;
 		}
+	}
 
-		return 0;
+	return 0;
 }
 
-int cmp_str(char (*cmp1)[10], char (*cmp2)[10], int num)
+int
+cmp_str(char (*cmp1)[10], char (*cmp2)[10], int num)
 {
-		int i;
-		for (i = 0; i < num; ++i){
-			if (strcmp(cmp1[i], cmp2[i]) != 0){
-					return 1;
-			}	
+	int i;
+	for (i = 0; i < num; ++i) {
+		if (strcmp(cmp1[i], cmp2[i]) != 0) {
+			return 1;
 		}
+	}
 
-		return 0;
+	return 0;
 }
 
-int 
+int
 main()
 {
-		FILE *fin,*fout;
-		char src[10][10];
-		char dst[10][10];
-		char tmp_in[10];
-		int i;
-		int flag = 0;
+	FILE *fin, *fout;
+	char src[10][10];
+	char dst[10][10];
+	char tmp_in[10];
+	int i;
+	int result = 7;
 
-		fin = fopen("transform.in","r");
-		fout = fopen("transform.out","w");
+	fin = fopen("transform.in", "r");
+	fout = fopen("transform.out", "w");
 
-		fscanf(fin, "%d", &num);
-		
-		for(i = 0; i < num; ++i){
-			fscanf(fin, "%s", tmp_in);
-			strcpy((char*)(src + i), tmp_in);
-			//printf("tmp_in is %s\n", tmp_in);		
-			//printf("src num is %s\n", src + i);		
-		}
-		
-		for(i = 0; i < num; ++i){
-			fscanf(fin, "%s", tmp_in);
-			strcpy((char*)(dst + i), tmp_in);
-			//printf("tmp_in is %s\n", tmp_in);		
-			//printf("src num is %s\n", dst + i);		
-		}
+	fscanf(fin, "%d", &num);
 
-		for(i = 0; i < 7; ++i){
-				switch(i){
-						case 0:
-							transform_90(src);
-							if (cmp(dst, num) == 0){
-									//printf("1\n");
-									fprintf(fout, "1\n");
-									flag = 1;
-							}
-							break;
-						case 1:
-							transform_90(src);
-							transform_90(test);
-							if (cmp(dst, num) == 0){
-									//printf("2\n");
-									fprintf(fout, "2\n");
-									flag = 1;
-							}
-							break;
-							//transform_180();
-						case 2:
-							transform_90(src);
-							transform_90(test);
-							transform_90(test);
-							if (cmp(dst, num) == 0){
-									//printf("3\n");
-									fprintf(fout, "3\n");
-									flag = 1;
-							}
-							break;
-							//transform_270();
-						case 3:
-							transform_reflect(src);
-							if (cmp(dst, num) == 0){
-									//printf("4\n");
-									fprintf(fout, "4\n");
-									flag = 1;
-							}
-							break;
-							//transform_inver();
-						case 4:
-							transform_90(src);
-							if (cmp(dst, num) == 0){
-									//printf("5\n");
-									fprintf(fout, "5\n");
-									flag = 1;
-							}
-							else{
-								transform_90(test);
-								if (cmp(dst, num) == 0){
-										//printf("5\n");
-										fprintf(fout, "5\n");
-										flag = 1;
-								}
-								else{
-									transform_90(test);
-									if (cmp(dst, num) == 0){
-											//printf("5\n");
-											fprintf(fout, "5\n");
-											flag = 1;
-									}
-								}
+	for (i = 0; i < num; ++i) {
+		fscanf(fin, "%s", tmp_in);
+		strcpy((char *) (src + i), tmp_in);
+	}
 
-							}
-							break;
-							//transform_comb();
-						case 5:
-								if(cmp_str(src, dst, num) == 0){
-									//printf("6\n");
-									fprintf(fout, "6\n");
-									flag = 1;
-								}
-							//transform_nothing();
-							break;
-						default:
-							break;
+	for (i = 0; i < num; ++i) {
+		fscanf(fin, "%s", tmp_in);
+		strcpy((char *) (dst + i), tmp_in);
+	}
+
+	for (i = 0; i < 7; ++i) {
+		switch (i) {
+		case 0:
+			transform_90(src);
+			if (cmp(dst, num) == 0) {
+				result = 1;
+			}
+			break;
+		case 1:
+			transform_90(src);
+			transform_90(test);
+			if (cmp(dst, num) == 0) {
+				result = 2;
+			}
+			break;
+		case 2:
+			transform_90(src);
+			transform_90(test);
+			transform_90(test);
+			if (cmp(dst, num) == 0) {
+				result = 3;
+			}
+			break;
+		case 3:
+			transform_reflect(src);
+			if (cmp(dst, num) == 0) {
+				result = 4;
+			}
+			break;
+		case 4:
+			transform_reflect(src);
+			transform_90(test);
+			if (cmp(dst, num) == 0) {
+				result = 5;
+			} else {
+				transform_90(test);
+				if (cmp(dst, num) == 0) {
+					result = 5;
+				} else {
+					transform_90(test);
+					if (cmp(dst, num) == 0) {
+						result = 5;
+					}
 				}
-				if (flag)
-					break;
+			}
+			break;
+		case 5:
+			if (cmp_str(src, dst, num) == 0) {
+				result = 6;
+			}
+			break;
+		default:
+			break;
 		}
+		if (result < 7)
+			break;
+	}
 
-		if (flag == 0){
-			//printf("7\n");
-			fprintf(fout, "7\n");
-		}
+	fprintf(fout, "%d\n", result);
 
-		exit(0);
+	exit(0);
 }
