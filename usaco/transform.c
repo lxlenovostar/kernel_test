49ef29693aeaa558a99b8f220a1d7f6cadd0eec0
lxlenovostar <lxlenovostar@gmail.com>
2014-11-24 06:59:36 +0800

    第一输出结果需要修改 第二5方案有误

--- usaco/transform.c
+++ usaco/transform.c
@@ -1,8 +1,8 @@
 /*
- *ID:lxlenovos1
- *LANG:C
- *TASK:transform
- **/
+ ID:lxlenovos1
+ LANG:C
+ TASK:transform
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,6 +10,28 @@
 
 int num;
 char test[10][10];
+
+/*
+ * 反射
+ **/
+void transform_reflect(char (*t_src)[10])
+{
+		int i, j, k;
+		char tmp[10][10];
+		
+		for (i = 0; i < num; ++i){
+			strncpy((char*)(tmp[i]), (const char *)(t_src + i), num);
+		}	
+
+		for (i = 0; i < num; ++i){
+				k = num - 1;
+				for (j = 0; j < num; ++j){
+						test[i][j] = tmp[i][k];
+						--k;
+				}
+		}
+
+}
 
 /*
  * 返回选择90度的图形
@@ -70,6 +92,18 @@ int cmp(char (*cmp)[10], int num)
 		return 0;
 }
 
+int cmp_str(char (*cmp1)[10], char (*cmp2)[10], int num)
+{
+		int i;
+		for (i = 0; i < num; ++i){
+			if (strcmp(cmp1[i], cmp2[i]) != 0){
+					return 1;
+			}	
+		}
+
+		return 0;
+}
+
 int 
 main()
 {
@@ -77,7 +111,8 @@ main()
 		char src[10][10];
 		char dst[10][10];
 		char tmp_in[10];
-		int i, j;
+		int i;
+		int flag = 0;
 
 		fin = fopen("transform.in","r");
 		fout = fopen("transform.out","w");
@@ -85,14 +120,14 @@ main()
 		fscanf(fin, "%d", &num);
 		
 		for(i = 0; i < num; ++i){
-			fscanf(fin, "%s", &tmp_in);
+			fscanf(fin, "%s", tmp_in);
 			strcpy((char*)(src + i), tmp_in);
 			//printf("tmp_in is %s\n", tmp_in);		
 			//printf("src num is %s\n", src + i);		
 		}
 		
 		for(i = 0; i < num; ++i){
-			fscanf(fin, "%s", &tmp_in);
+			fscanf(fin, "%s", tmp_in);
 			strcpy((char*)(dst + i), tmp_in);
 			//printf("tmp_in is %s\n", tmp_in);		
 			//printf("src num is %s\n", dst + i);		
@@ -102,27 +137,86 @@ main()
 				switch(i){
 						case 0:
 							transform_90(src);
-							if (cmp(dst, num) == 0)
-									printf("1\n");
+							if (cmp(dst, num) == 0){
+									//printf("1\n");
+									fprintf(fout, "1\n");
+									flag = 1;
+							}
 							break;
 						case 1:
+							transform_90(src);
+							transform_90(test);
+							if (cmp(dst, num) == 0){
+									//printf("2\n");
+									fprintf(fout, "2\n");
+									flag = 1;
+							}
+							break;
 							//transform_180();
-							break;
 						case 2:
+							transform_90(src);
+							transform_90(test);
+							transform_90(test);
+							if (cmp(dst, num) == 0){
+									//printf("3\n");
+									fprintf(fout, "3\n");
+									flag = 1;
+							}
+							break;
 							//transform_270();
-							break;
 						case 3:
+							transform_reflect(src);
+							if (cmp(dst, num) == 0){
+									//printf("4\n");
+									fprintf(fout, "4\n");
+									flag = 1;
+							}
+							break;
 							//transform_inver();
-							break;
 						case 4:
-							//transform_comb();
+							transform_90(src);
+							if (cmp(dst, num) == 0){
+									//printf("5\n");
+									fprintf(fout, "5\n");
+									flag = 1;
+							}
+							else{
+								transform_90(test);
+								if (cmp(dst, num) == 0){
+										//printf("5\n");
+										fprintf(fout, "5\n");
+										flag = 1;
+								}
+								else{
+									transform_90(test);
+									if (cmp(dst, num) == 0){
+											//printf("5\n");
+											fprintf(fout, "5\n");
+											flag = 1;
+									}
+								}
+
+							}
 							break;
+							//transform_comb();
 						case 5:
+								if(cmp_str(src, dst, num) == 0){
+									//printf("6\n");
+									fprintf(fout, "6\n");
+									flag = 1;
+								}
 							//transform_nothing();
 							break;
 						default:
 							break;
 				}
+				if (flag)
+					break;
+		}
+
+		if (flag == 0){
+			//printf("7\n");
+			fprintf(fout, "7\n");
 		}
 
 		exit(0);
