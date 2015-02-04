658bfb586c6e00311c15fc4afdd2a14ca765b6cd
lxlenovostar <lxlenovostar@gmail.com>
2015-02-04 23:17:40 +0800

    作业1

---
+++ coursera/algs4/Percolation.java
@@ -0,0 +1,94 @@
+/*************************************************************************
+ *  author:    lx
+ **************************************************************************/
+
+public class Percolation {
+    private boolean[] site;
+    private WeightedQuickUnionUF uf;
+    private int count = 0;
+    private int top = 0;
+    private int bottom = 1;
+
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N)
+    {
+        int i, j;
+
+        if (N <= 0)
+            throw new java.lang.IllegalArgumentException("N is not a fit value");
+
+        count = N;
+        site = new boolean[N];
+        uf = new WeightedQuickUnionUF(N*N + 2);
+
+        // handle the first and last row.
+        for (i = 1; i <= N; ++i) {
+            uf.union(top, xyTo1D(1, i) + 2);
+            uf.union(bottom, xyTo1D(count, i) + 2);
+        }
+    }
+
+    private int xyTo1D(int i, int j)
+    {
+        validate(i, j);
+        return (i - 1) * count + j - 1;
+    }
+
+    private void validate(int i, int j)
+    {
+        if (i < 1 || i > count || j < 1 || j > count)
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+    }
+
+    private void unionTwoSites(int i, int j, int m, int n)
+    {
+        uf.union(xyTo1D(i, j) + 2, xyTo1D(m, n) + 2);
+    }
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j)
+    {
+        validate(i, j);
+
+        if (isOpen(i, j))
+            return;
+
+        site[xyTo1D(i, j)] = true;
+
+        if (i > 1 && isOpen(i-1, j)) // top
+            unionTwoSites(i, j, i-1, j);
+        if (i < count && isOpen(i+1, j)) // bottom
+            unionTwoSites(i, j, i+1, j);
+        if (j > 1 && isOpen(i, j-1)) // left
+            unionTwoSites(i, j, i, j-1);
+        if (j < count && isOpen(i, j+1)) // right
+            unionTwoSites(i, j, i, j+1);
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j)
+    {
+        validate(i, j);
+
+        return site[xyTo1D(i, j)];
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j)
+    {
+        return isOpen(i, j) && uf.connected(top, xyTo1D(i, j)+2);
+    }
+
+    // does the system percolate?
+    public boolean percolates()
+    {
+        return uf.connected(top, bottom);
+    }
+
+    // test client (optional)}
+    public static void main(String[] args)
+    {
+        Percolation t = new Percolation(50);
+        t.open(1, 3);
+    }
+}
---
+++ coursera/algs4/PercolationStats.java
@@ -0,0 +1,89 @@
+public class PercolationStats
+{
+    private double[] fractions;
+    private int N;
+    private int T;
+
+    public PercolationStats(int N, int T)
+    {
+        this.N = N;
+        this.T = T;
+
+        fractions = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            double frac = (double) oneTime() / (N * N);
+            fractions[i] = frac;
+        }
+    }
+
+    public double mean()
+    {
+        return StdStats.mean(fractions);
+    }
+
+    public double stddev()
+    {
+        return StdStats.stddev(fractions);
+    }
+
+    private int oneTime()
+    {
+        Percolation perc = new Percolation(N);
+        int sitesOpened = 0;
+
+        while (!perc.percolates()) {
+            int i = rand();
+            int j = rand();
+
+            if (!perc.isOpen(i, j)) {
+                perc.open(i, j);
+                sitesOpened++;
+            }
+        }
+        return sitesOpened;
+    }
+
+    private int rand()
+    {
+        return StdRandom.uniform(N) + 1;
+    }
+
+    public double confidenceLo()
+    {
+        double mean = this.mean();
+        double std = this.stddev();
+        double left = mean - 1.96 * std / Math.sqrt(T);
+
+        return left;
+    }
+
+    public double confidenceHi()
+    {
+        double mean = this.mean();
+        double std = this.stddev();
+        double right = mean + 1.96 * std / Math.sqrt(T);
+
+        return right;
+    }
+
+    public static void main(String[] args)
+    {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        PercolationStats stat = new PercolationStats(N, T);
+        double mean = stat.mean();
+        double std = stat.stddev();
+        double left = mean - 1.96 * std / Math.sqrt(T);
+        double right = mean + 1.96 * std / Math.sqrt(T);
+        //int leftN = (int) (N * N * left);
+        //int rightN = (int) (N * N * right);
+
+        StdOut.printf("%-23s = %f\n", "mean", mean);
+        StdOut.printf("%-23s = %f\n", "stddev", std);
+        StdOut.printf("%-23s = %f, %f\n", "95% confidence interval", left, right);
+        //StdOut.printf("\nN: %d, T: %d\n", N, T);
+        //StdOut.printf("Total: %d, open sites [%d, %d]\n",N*N, leftN, rightN);
+    }
+}
