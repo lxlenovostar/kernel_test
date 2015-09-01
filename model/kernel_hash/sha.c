002a92edba817a3dfa469f1c9e0d5e8d6996c4be
root <root@localhost.localdomain>
2015-08-31 18:46:31 -0700

    调整计算SHA-1 API的位置

--- model/kernel_hash/sha.c
+++ model/kernel_hash/sha.c
@@ -1,21 +1,27 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/crypto.h>
 #include <linux/err.h>
 #include <linux/scatterlist.h>
-#include "debug.h"
-MODULE_LICENSE("Dual BSD/GPL");
+#include "sha.h"
 
-#define SHA1_LENGTH     20
-
-static int hello_init(void)
-{
+/**
+ * ecryptfs_calculate_sha - calculates the SHA-1 of @src
+ * @dst: Pointer to 20 bytes of allocated memory
+ * @src: Data to be SHA-1'd
+ * @len: Length of @src
+ *
+ * Uses the allocated crypto context that crypt_stat references to
+ * generate the MD5 sum of the contents of src.
+ */
+int ecryptfs_calculate_sha1(char *dst, char *src, int len) {
 	/*
      * http://lxr.oss.org.cn/source/fs/ecryptfs/crypto.c?v=2.6.30
      */
 	struct scatterlist sg;
 	struct hash_desc desc;
+	
+	int rc = 0;
+	int i; 
+	//char hashtext[SHA1_LENGTH];
 
 	//way 1		
 	/*
@@ -41,19 +47,17 @@ static int hello_init(void)
 	*/
 
 	// way 4.
+	/*
 	char *plaintext = (char *)__get_free_page(GFP_KERNEL);
 	memcpy(plaintext, "c", 1);
 	size_t len = 1;
+	*/
 
-	int rc = 0;
-	int i; 
-	char hashtext[SHA1_LENGTH];
+    //memset(hashtext, 0x00, SHA1_LENGTH);
+    //printk(KERN_INFO "sha1: %s\n", __FUNCTION__);
+	//printk(KERN_INFO "valid=%s PAGE=%lu, plaintext=%lu, %d , %s\n", virt_addr_valid(plaintext) ? "true" : "false", PAGE_OFFSET, (unsigned long)plaintext, (unsigned long)plaintext - PAGE_OFFSET, ((unsigned long)plaintext > __START_KERNEL_map) ? "true" : "false");
 
-    memset(hashtext, 0x00, SHA1_LENGTH);
-    printk(KERN_INFO "sha1: %s\n", __FUNCTION__);
-	printk(KERN_INFO "valid=%s PAGE=%lu, plaintext=%lu, %d , %s\n", virt_addr_valid(plaintext) ? "true" : "false", PAGE_OFFSET, (unsigned long)plaintext, (unsigned long)plaintext - PAGE_OFFSET, ((unsigned long)plaintext > __START_KERNEL_map) ? "true" : "false");
-
-	sg_init_one(&sg, plaintext, len);
+	sg_init_one(&sg, (u8 *)src, len);
 	desc.tfm = crypto_alloc_hash("sha1", 0, CRYPTO_ALG_ASYNC);
 	desc.flags = 0;
 	
@@ -67,7 +71,7 @@ static int hello_init(void)
     	printk(KERN_ERR "%s: Error updating crypto hash; rc = [%d]\n", __func__, rc);
         goto out;
     }
-	rc = crypto_hash_final(&desc, hashtext);
+	rc = crypto_hash_final(&desc, dst);
 	if (rc) {
     	printk(KERN_ERR "%s: Error finalizing crypto hash; rc = [%d]\n", __func__, rc);
         goto out;
@@ -75,18 +79,10 @@ static int hello_init(void)
 	crypto_free_hash(desc.tfm);
     
 	for (i = 0; i < 20; i++) {
-        printk(KERN_INFO "%02x-%d\n", hashtext[i]&0xff, i);
+        printk(KERN_INFO "%02x-%d\n", dst[i]&0xff, i);
     }
 
 out:
-    printk(KERN_INFO "end\n");
     return rc;
 }
 
-static void hello_exit(void)
-{
-    printk(KERN_ALERT "Goodbye, cruel world\n");
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
---
+++ model/kernel_hash/sha.h
@@ -0,0 +1,1 @@
+extern int ecryptfs_calculate_sha1(char *dst, char *src, int len);
