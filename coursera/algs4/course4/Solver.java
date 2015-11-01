81a2770a47f7b8da0eed6d72de5cf0b8f3796894
root <root@localhost.localdomain>
2015-11-01 10:17:51 +0800

    solver 没有完成

--- coursera/algs4/course4/Solver.java
+++ coursera/algs4/course4/Solver.java
@@ -1,35 +1,29 @@
 public class Solver {
+    private finally boolean solved;
+    private finally int move_count; 
+
     public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    
+    {
+        this.solved = false;
+        this.move_count = 0;
+
+        Board 
+    }    
+
     public boolean isSolvable()            // is the initial board solvable?
-    
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    
-    public static void main(String[] args) // solve a slider puzzle (given below)
-}
-
-public static void main(String[] args) {
-
-    // create initial board from file
-    In in = new In(args[0]);
-    int N = in.readInt();
-    int[][] blocks = new int[N][N];
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            blocks[i][j] = in.readInt();
-    Board initial = new Board(blocks);
-
-    // solve the puzzle
-    Solver solver = new Solver(initial);
-
-    // print solution to standard output
-    if (!solver.isSolvable())
-        StdOut.println("No solution possible");
-    else {
-        StdOut.println("Minimum number of moves = " + solver.moves());
-        for (Board board : solver.solution())
-            StdOut.println(board);
+    {
+        return this.solved;
     }
+
+    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+    {
+        return this.move_count;
+    }
+
+    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+    {
+
+    }
+
+    //public static void main(String[] args) // solve a slider puzzle (given below)
 }
