d0ebd06693494e2c95a31cd419858935c3c6ea16
root <root@localhost.localdomain>
2015-11-19 02:20:07 -0800

    传入协议栈依然有问题，待解决

--- test/hook_wq_tk/main.c
+++ test/hook_wq_tk/main.c
@@ -10,6 +10,114 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
+#include <net/tcp.h>
+#include <linux/interrupt.h>
+#include <linux/skbuff.h>
+#include "ws_st_symbols.h"
+
+static struct workqueue_struct *my_wq;
+
+typedef struct {
+	struct work_struct my_work;
+	int    index;
+} my_work_t;
+
+DEFINE_PER_CPU(struct work_struct , work); 
+
+struct reject_skb {
+	void *skb;
+	struct list_head list;
+};
+
+static DEFINE_PER_CPU(struct list_head, head_skb_list);
+
+#define CACHE_NAME "skb_cache"
+static struct kmem_cache * hash_cachep/* __read_mostly*/;
+
+void my_tasklet_function(unsigned long data)
+{
+	struct sk_buff *skb = (struct sk_buff *)data;
+
+	struct iphdr *iph;
+	struct tcphdr *tcph;
+	unsigned short sport, dport;
+	__be32 saddr, daddr;
+	char dsthost[16];
+	
+	iph = (struct iphdr *)skb->data;
+	tcph = (struct tcphdr *)(skb->data + (iph->ihl << 2));
+
+	sport = tcph->source;
+	dport = tcph->dest;
+	saddr = iph->saddr;
+	daddr = iph->daddr;
+		
+	snprintf(dsthost, 16, "%pI4", &saddr);
+
+	if (!strcmp(dsthost, "192.168.27.77")) {  
+		printk(KERN_INFO "Im here end0.");
+	
+	//kfree_skb(skb);
+	//return;
+	
+	//printk(KERN_INFO "Im here end.");
+	//return;
+	
+	//spinlock_t lock = SPIN_LOCK_UNLOCKED;
+	//spin_lock_bh(&lock);
+	//netif_receive_skb(skb);
+	//ip_rcv_finish(skb);
+	//spin_unlock_bh(&lock);
+	//local_bh_disable();
+	(*tcp_v4_rcv_ptr)(skb);
+	//local_bh_enable();
+	kfree_skb(skb);
+	}
+	printk(KERN_INFO "Im here end1.");
+	return;
+}
+
+static void handle_skb(struct work_struct *work)
+{
+	int cpu;
+    struct reject_skb *cp, *next;
+	int threshold = 10;
+	int i = 0;	
+	
+	struct tasklet_struct *my_tasklet;
+	LIST_HEAD(hand_list);
+
+	cpu = get_cpu();
+	local_bh_disable();
+    list_for_each_entry_safe(cp, next, &per_cpu(head_skb_list, cpu), list) {
+		if (i >= threshold)
+			break;
+		else
+			++i;
+
+		list_del(&cp->list);
+		list_add_tail(&cp->list, &hand_list);
+	}
+	local_bh_enable();
+	put_cpu();
+		
+	my_tasklet = kmalloc(sizeof(struct tasklet_struct), GFP_ATOMIC);
+    list_for_each_entry_safe(cp, next, &hand_list, list) {
+		list_del(&cp->list);
+		
+		tasklet_init(my_tasklet, my_tasklet_function, (unsigned long)cp->skb);
+		/* Schedule the Bottom Half */
+		tasklet_hi_schedule(my_tasklet);
+		tasklet_kill( my_tasklet );
+		
+		kmem_cache_free(hash_cachep, cp);
+	}
+	kfree(my_tasklet);
+	
+	cpu = get_cpu();
+	printk(KERN_INFO "cpu %d is run workqueue", cpu);
+	put_cpu();
+}
 
 unsigned int
 hook_local_in(unsigned int hooknum, struct sk_buff *skb,
@@ -22,12 +130,8 @@ hook_local_in(unsigned int hooknum, struct sk_buff *skb,
 	__be32 saddr, daddr;
 	char dsthost[16];
 	char *verify = "pack";
-	int i;
+	int cpu;
 
-
-	/*
-     * TODO: this maybe need fix it.
-     */
 	skb_linearize(skb);
 	
 	iph = (struct iphdr *)skb->data;
@@ -39,24 +143,44 @@ hook_local_in(unsigned int hooknum, struct sk_buff *skb,
 		saddr = iph->saddr;
 		daddr = iph->daddr;
 		
-		snprintf(dsthost, 16, "%pI4", &daddr);
+		snprintf(dsthost, 16, "%pI4", &saddr);
 
-		/*
-		 * 此段代码用于控制 SKB 的重入   
-         */
-		/*
-		memcpy(skb->cb, "pack", 4);
-		printk(KERN_INFO "len is:%d", strlen(skb->cb));
-		for (i = 0; i < strlen(skb->cb); i++)
-			printk(KERN_INFO "c is:%c", skb->cb[i]);
-		
-		if (!memcmp(skb->cb, "pack", 4))
-			printk(KERN_INFO "true");
-		*/
-		
-		//return NF_STOLEN;
+		if (!strcmp(dsthost, "192.168.27.77")) { 
+
+			if (!memcmp(skb->cb, verify, 4)) {
+				printk(KERN_INFO "true");
+				return NF_ACCEPT;
+			} else {
+				memcpy(skb->cb, verify, 4);
+				
+				struct reject_skb *skb_item = kmem_cache_zalloc(hash_cachep, GFP_ATOMIC);  
+   				if (skb_item == NULL) {
+   					printk(KERN_INFO "%s\n", __FUNCTION__);
+       				BUG();
+   				}
+				skb_item->skb = skb_clone(skb, GFP_ATOMIC);
+				INIT_LIST_HEAD(&skb_item->list);   
+
+				//SKB 进入等待队列
+				cpu = get_cpu();
+				list_add_tail(&skb_item->list, &per_cpu(head_skb_list, cpu));
+				put_cpu();
+				
+				//判断是否开启工作队列
+				cpu = get_cpu();
+				if (!work_pending(&(per_cpu(work, cpu)))) {
+					INIT_WORK(&(per_cpu(work, cpu)), handle_skb);
+					//(struct work_struct *)(per_cpu(work, cpu))->index = cpu;
+					queue_work(my_wq, &(per_cpu(work, cpu)));
+				} 
+				put_cpu();
+			}
+			//return NF_STOLEN;
+			//return NF_DROP;
+			return NF_ACCEPT;
+		}
 	}
-
+	
 	return NF_ACCEPT;
 }
 
@@ -78,16 +202,41 @@ static struct nf_hook_ops hook_ops[] = {
 };
 
 
-
 static int minit(void)
 {
-	int ret;
+	int ret, cpu;
+
+	for_each_online_cpu(cpu) {
+		INIT_LIST_HEAD(&per_cpu(head_skb_list, cpu));
+	}
 
 	ret = nf_register_hooks(hook_ops, ARRAY_SIZE(hook_ops));
 	if (ret) {
 		printk("local_in hooks failed\n");
 	}
 	
+	my_wq = create_workqueue("my_queue");
+	if (!my_wq)
+		return -1;
+
+#if ( LINUX_VERSION_CODE < KERNEL_VERSION(2,6,25) )
+    hash_cachep = kmem_cache_create(CACHE_NAME,
+            sizeof(struct reject_skb),
+            0, SLAB_HWCACHE_ALIGN, NULL, NULL);
+#else
+    hash_cachep = kmem_cache_create(CACHE_NAME,
+            sizeof(struct reject_skb),
+            0, SLAB_HWCACHE_ALIGN, NULL);
+#endif
+
+    if (!hash_cachep) {
+        printk(KERN_ERR "****** %s : kmem_cache_create  error\n",
+                __FUNCTION__);
+        return -ENOMEM;
+    }
+
+
+
 	printk("Start %s.\n", THIS_MODULE->name);
 
 	return 0;
@@ -95,7 +244,12 @@ static int minit(void)
 
 static void mexit(void)
 {
+	flush_workqueue( my_wq );
+	destroy_workqueue( my_wq );
+	
 	nf_unregister_hooks(hook_ops, ARRAY_SIZE(hook_ops));
+	kmem_cache_destroy(hash_cachep);
+	
 	printk("Exit %s.\n", THIS_MODULE->name);
 }
 
