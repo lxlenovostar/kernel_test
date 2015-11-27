678c1a10431d3dec8a57f2a3cb647d7cefcad202
root <root@localhost.localdomain>
2015-11-26 23:50:26 -0800

    验证缓存比实验中使用

---
+++ test/sha/wssha.mod.c
@@ -0,0 +1,41 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+ .name = KBUILD_MODNAME,
+ .init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+ .exit = cleanup_module,
+#endif
+ .arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x14522340, "module_layout" },
+	{ 0xea147363, "printk" },
+	{ 0xb4390f9a, "mcount" },
+	{ 0xf0fdf6cb, "__stack_chk_fail" },
+	{ 0x7c2458f8, "crypto_destroy_tfm" },
+	{ 0xb6244511, "sg_init_one" },
+	{ 0x57b1dc2b, "crypto_alloc_base" },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "B8D7F3ECC7C2D0438A71BA8");
+
+static const struct rheldata _rheldata __used
+__attribute__((section(".rheldata"))) = {
+	.rhel_major = 6,
+	.rhel_minor = 5,
+};
