7cf8dad734d3b0b16df0dc764f7d7c09968b944b
root <root@localhost.localdomain>
2015-12-07 02:19:39 -0800

    SKB需要经过路由处理。目前处理大量数据依然有BUG。

--- test/hook_wq_tk/main.c
+++ test/hook_wq_tk/main.c
@@ -33,39 +33,22 @@ struct reject_skb {
 static DEFINE_PER_CPU(struct list_head, head_skb_list);
 
 #define CACHE_NAME "skb_cache"
-static struct kmem_cache * hash_cachep/* __read_mostly*/;
+//static struct kmem_cache * hash_cachep/* __read_mostly*/;
+//static DEFINE_PER_CPU(struct kmem_cache *, hash_cachep);
+static struct kmem_cache **hash_cachep;
 
 void my_tasklet_function(unsigned long data)
 {
 	struct sk_buff *skb = (struct sk_buff *)data;
 
-	struct iphdr *iph;
-	struct tcphdr *tcph;
-	unsigned short sport, dport;
-	__be32 saddr, daddr;
-	char dsthost[16];
+	printk(KERN_INFO "Im here end0.");
+	local_bh_disable();
+   	//skb_pull(skb, ip_hdrlen(skb));
+	//skb_reset_transport_header(skb);
+	(*tcp_v4_rcv_ptr)(skb);
+	local_bh_enable();
+	printk(KERN_INFO "Im here end1.");
 	
-	iph = (struct iphdr *)skb->data;
-	tcph = (struct tcphdr *)(skb->data + (iph->ihl << 2));
-
-	sport = tcph->source;
-	dport = tcph->dest;
-	saddr = iph->saddr;
-	daddr = iph->daddr;
-		
-	snprintf(dsthost, 16, "%pI4", &saddr);
-
-	//if (!strcmp(dsthost, "192.168.27.77")) {  
-	if (strcmp(dsthost, "139.209.90.60") == 0 && ntohs(sport) == 80) {  
-		//printk(KERN_INFO "skb->len1 is:%d", skb->len);
-		printk(KERN_INFO "Im here end0.");
-		local_bh_disable();
-        skb_pull(skb, ip_hdrlen(skb));
-		skb_reset_transport_header(skb);
-		(*tcp_v4_rcv_ptr)(skb);
-		local_bh_enable();
-		printk(KERN_INFO "Im here end1.");
-	}
 	return;
 }
 
@@ -100,9 +83,12 @@ static void handle_skb(struct work_struct *work)
 		tasklet_init(my_tasklet, my_tasklet_function, (unsigned long)cp->skb);
 		/* Schedule the Bottom Half */
 		tasklet_hi_schedule(my_tasklet);
-		tasklet_kill( my_tasklet );
+		tasklet_kill(my_tasklet);
 		
-		kmem_cache_free(hash_cachep, cp);
+		//kmem_cache_free(per_cpu(hash_cachep, cpu), cp);
+		cpu = get_cpu();
+		kmem_cache_free(*(hash_cachep + cpu), cp);
+		put_cpu();
 	}
 	kfree(my_tasklet);
 	
@@ -113,11 +99,6 @@ static void handle_skb(struct work_struct *work)
 	*/
 }
 
-/*unsigned int
-hook_local_in(unsigned int hooknum, struct sk_buff *skb,
-	      const struct net_device *in, const struct net_device *out,
-	      int (*okfn) (struct sk_buff *))
-*/
 int jpf_ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt, struct net_device *orig_dev)
 {
 	struct iphdr *iph;
@@ -125,6 +106,7 @@ int jpf_ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *
 	unsigned short sport, dport;
 	__be32 saddr, daddr;
 	char dsthost[16];
+	char ssthost[16];
 	int cpu;
 
 	skb_linearize(skb);
@@ -139,11 +121,18 @@ int jpf_ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *
 		daddr = iph->daddr;
 		
 		snprintf(dsthost, 16, "%pI4", &daddr);
+		snprintf(ssthost, 16, "%pI4", &saddr);
+		int data_len = ntohs(iph->tot_len) - (iph->ihl << 2) - (tcph->doff << 2);
 
-		//if (!strcmp(dsthost, "192.168.27.77")) { 
-		if (strcmp(dsthost, "139.209.90.60") == 0 && ntohs(sport) == 80) {  
+		//if (!strcmp(ssthost, "192.168.27.77")) { 
+		//if (strcmp(dsthost, "139.209.90.60") == 0 && ntohs(sport) == 80) {  
+		//if (strcmp(dsthost, "139.209.90.213") == 0) {  
+		if (strcmp(ssthost, "45.78.40.252") == 0 && data_len > 0) {  
+		//if (strcmp(dsthost, "139.209.90.60") == 0 || strcmp(ssthost, "139.209.90.60") == 0) {  
 				//printk(KERN_INFO "skb->len0 is:%d", skb->len);
-				struct reject_skb *skb_item = kmem_cache_zalloc(hash_cachep, GFP_ATOMIC);  
+				cpu = get_cpu();
+				struct reject_skb *skb_item = kmem_cache_zalloc(*(hash_cachep + cpu), GFP_ATOMIC);  
+				put_cpu();
    				if (!skb_item) {
    					printk(KERN_INFO "%s\n", __FUNCTION__);
        				BUG();
@@ -167,13 +156,70 @@ int jpf_ip_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *
 	}
 	jprobe_return();
 	return 0;
-			
-	//return NF_STOLEN;
-	//return NF_DROP;
-	//return NF_ACCEPT;
 }
 
-/*
+unsigned int
+hook_local_in(unsigned int hooknum, struct sk_buff *skb,
+	      const struct net_device *in, const struct net_device *out,
+	      int (*okfn) (struct sk_buff *))
+{
+	return NF_ACCEPT;
+	
+	/*
+	struct iphdr *iph;
+	struct tcphdr *tcph;
+	unsigned short sport, dport;
+	__be32 saddr, daddr;
+	char dsthost[16];
+	char ssthost[16];
+	int cpu;
+
+	skb_linearize(skb);
+	
+	iph = (struct iphdr *)skb->data;
+	tcph = (struct tcphdr *)(skb->data + (iph->ihl << 2));
+
+	if (iph->protocol == IPPROTO_TCP) {
+		sport = tcph->source;
+		dport = tcph->dest;
+		saddr = iph->saddr;
+		daddr = iph->daddr;
+		
+		snprintf(dsthost, 16, "%pI4", &daddr);
+		snprintf(ssthost, 16, "%pI4", &saddr);
+
+		//if (!strcmp(ssthost, "192.168.27.77")) { 
+		if (strcmp(dsthost, "139.209.90.60") == 0 && ntohs(sport) == 80) {  
+				//printk(KERN_INFO "skb->len0 is:%d", skb->len);
+				struct reject_skb *skb_item = kmem_cache_zalloc(hash_cachep, GFP_ATOMIC);  
+   				if (!skb_item) {
+   					printk(KERN_INFO "%s\n", __FUNCTION__);
+       				BUG();
+   				}
+				skb_item->skb = skb_copy(skb, GFP_ATOMIC);
+				INIT_LIST_HEAD(&skb_item->list);   
+
+				//SKB 进入等待队列
+				cpu = get_cpu();
+				list_add_tail(&skb_item->list, &per_cpu(head_skb_list, cpu));
+				put_cpu();
+				
+				//判断是否开启工作队列
+				cpu = get_cpu();
+				if (!work_pending(&(per_cpu(work, cpu)))) {
+					INIT_WORK(&(per_cpu(work, cpu)), handle_skb);
+					queue_work(my_wq, &(per_cpu(work, cpu)));
+				} 
+				put_cpu();
+	
+				return NF_DROP;
+		}
+	}
+	//return NF_STOLEN;
+	return NF_ACCEPT;
+	*/
+}
+
 static struct nf_hook_ops hook_ops[] = {
 	{
 	 .owner = THIS_MODULE,
@@ -185,11 +231,10 @@ static struct nf_hook_ops hook_ops[] = {
 #else
 	 .hook = hook_local_in,
 	 .hooknum      = NF_INET_LOCAL_IN,
-	 //.hooknum = NF_INET_PRE_ROUTING,
+	 .hooknum = NF_INET_PRE_ROUTING,
 #endif
 	 },
 };
-*/
 
 struct jprobe jps_netif_receive_skb = { 
     //.entry = jpf_netif_receive_skb,
@@ -201,6 +246,16 @@ struct jprobe jps_netif_receive_skb = {
 
 static int minit(void)
 {
+	/*
+	int cpu;
+	char cachename[20];
+	for (cpu = 0; cpu < num_online_cpus(); cpu++)  {
+		memset(cachename, '\0', sizeof(cachename));
+		sprintf(cachename, "cacheskb%d", cpu);
+	}
+	return 0;
+	*/
+
 	int ret, cpu;
 
 	for_each_online_cpu(cpu) {
@@ -213,35 +268,33 @@ static int minit(void)
         return -1;
     }
  
-	/*
 	ret = nf_register_hooks(hook_ops, ARRAY_SIZE(hook_ops));
 	if (ret) {
 		printk("local_in hooks failed\n");
 	}
-	*/
 
 	my_wq = create_workqueue("my_queue");
 	if (!my_wq)
 		return -1;
 
-#if ( LINUX_VERSION_CODE < KERNEL_VERSION(2,6,25) )
-    hash_cachep = kmem_cache_create(CACHE_NAME,
-            sizeof(struct reject_skb),
-            0, SLAB_HWCACHE_ALIGN, NULL, NULL);
-#else
-    hash_cachep = kmem_cache_create(CACHE_NAME,
-            sizeof(struct reject_skb),
-            0, SLAB_HWCACHE_ALIGN, NULL);
-#endif
+	hash_cachep = kmalloc(sizeof(struct kmem_cache *)*num_online_cpus(), GFP_ATOMIC);
+	char cachename[20];
+	for (cpu = 0; cpu < num_online_cpus(); cpu++)  {
+		memset(cachename, '\0', sizeof(cachename));
+		sprintf(cachename, "cacheskb%d", cpu);
 
-    if (!hash_cachep) {
-        printk(KERN_ERR "****** %s : kmem_cache_create  error\n",
+	#if ( LINUX_VERSION_CODE < KERNEL_VERSION(2,6,25) )
+    	*(hash_cachep + cpu) = kmem_cache_create(cachename, sizeof(struct reject_skb), 0, SLAB_HWCACHE_ALIGN, NULL, NULL);
+	#else
+    	*(hash_cachep + cpu) = kmem_cache_create(cachename, sizeof(struct reject_skb), 0, SLAB_HWCACHE_ALIGN, NULL);
+	#endif
+
+    	if (!*(hash_cachep + cpu)) {
+        	printk(KERN_ERR "****** %s : kmem_cache_create  error\n",
                 __FUNCTION__);
         return -ENOMEM;
-    }
-
-
-
+    	}
+	}
 	printk("Start %s.\n", THIS_MODULE->name);
 
 	return 0;
@@ -249,13 +302,17 @@ static int minit(void)
 
 static void mexit(void)
 {
-	flush_workqueue( my_wq );
-	destroy_workqueue( my_wq );
+	int cpu;
+
+	flush_workqueue(my_wq);
+	destroy_workqueue(my_wq);
 	
     unregister_jprobe(&jps_netif_receive_skb);
-	//nf_unregister_hooks(hook_ops, ARRAY_SIZE(hook_ops));
-	kmem_cache_destroy(hash_cachep);
-	
+	nf_unregister_hooks(hook_ops, ARRAY_SIZE(hook_ops));
+	for (cpu = 0; cpu < num_online_cpus(); cpu++) {
+		kmem_cache_destroy(*(hash_cachep + cpu));
+	}	
+	kfree(hash_cachep);
 	printk("Exit %s.\n", THIS_MODULE->name);
 }
 
