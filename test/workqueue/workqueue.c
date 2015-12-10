4537c89d313cc58e02df4e9911c0d4c0ffa5233f
root <root@localhost.localdomain>
2015-12-09 19:07:44 -0800

    实验验证：slab是否可作为文件读的缓冲区

--- test/workqueue/workqueue.c
+++ test/workqueue/workqueue.c
@@ -1,13 +1,17 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/err.h>
 #include <linux/workqueue.h>
 #include <linux/syscalls.h>
 #include <linux/file.h>
 
+#define CHUNKSTEP 32
+#define CACHE_SLAB_CHUNK1 "slab_chunk_1"
+struct kmem_cache *slab_chunk1;
+ 
 static struct workqueue_struct *my_wq;
-
 typedef struct {
 	struct work_struct my_work;
 	int    x;
@@ -38,6 +42,7 @@ static void my_wq_read( struct work_struct *work)
 	printk( "my_work.x %d\n", my_work->x );
 	kfree( (void *)work );
 
+	char *tmp_read = kmem_cache_zalloc(slab_chunk1, GFP_ATOMIC);
     unsigned char *buffer = kzalloc(1024, GFP_KERNEL);
     struct file *file;
     int result=0;
@@ -50,20 +55,24 @@ static void my_wq_read( struct work_struct *work)
    file = filp_open("/opt/lx1", O_RDONLY, 0);
    if (IS_ERR(file)) {
    		kfree(buffer);
+		 kmem_cache_free(slab_chunk1, tmp_read);
         printk("Config_FileOperation:open file fail?\n");
         //return -1;
   	}
- 
-    if (kernel_read(file, 0, buffer, 1024) < 0) {
+	 
+    //if (kernel_read(file, 0, buffer, 1024) < 0) {
+    if (kernel_read(file, 0, tmp_read, 1024) < 0) {
     	printk("read file error?\n");
         result = -1;
         goto error1;
     }
  
-	printk( "content is:%s\n", buffer);
+	//printk( "content is:%s\n", buffer);
+	printk(KERN_INFO "content is:%s", tmp_read);
  
 error1:
          kfree(buffer);
+		 kmem_cache_free(slab_chunk1, tmp_read);
          fput(file);
 
 	return;
@@ -76,19 +85,19 @@ static void my_wq_write( struct work_struct *work)
 	printk( "my_work.x %d\n", my_work->x );
 	kfree( (void *)work );
 	
-	struct file *file = filp_open("/opt/lx", O_RDWR | O_CREAT | O_TRUNC, 0600);
+	struct file *file = filp_open("/opt/lx1", O_RDWR | O_CREAT | O_TRUNC, 0600);
     int ret;
- 	char *md_buf = "hello world.";
+ 	char *md_buf = "1hello world1.";
 	int md_buf_len = strlen(md_buf);
 
     if (IS_ERR(file)) {
-    	pr_err("filp_open(%s) for failed\n", "/opt/lx");
+    	pr_err("filp_open(%s) for failed\n", "/opt/lx1");
         //return -ENODEV;
     }
     
 	ret = kernel_write(file, md_buf, md_buf_len, 0);
     if (ret < 0)
-    	pr_err("Error writing file: %s\n", "/opt/lx");
+    	pr_err("Error writing file: %s\n", "/opt/lx1");
         
 	fput(file);
          
@@ -100,6 +109,22 @@ static void my_wq_write( struct work_struct *work)
 static int minit(void)
 {
 	printk("Start %s.\n", THIS_MODULE->name);
+
+	#if ( LINUX_VERSION_CODE < KERNEL_VERSION(2,6,25) )
+    slab_chunk1 = kmem_cache_create(CACHE_SLAB_CHUNK1, 
+            CHUNKSTEP,
+            0, SLAB_HWCACHE_ALIGN, NULL, NULL);
+    #else
+    slab_chunk1 = kmem_cache_create(CACHE_SLAB_CHUNK1,
+            CHUNKSTEP,
+            0, SLAB_HWCACHE_ALIGN, NULL);
+    #endif
+
+    if (!slab_chunk1) {
+        printk(KERN_ERR "****** %s : kmem_cache_create for chunk1  error\n",
+                __FUNCTION__);
+        return -ENOMEM;
+    }   
 
 	my_wq = create_workqueue("my_queue");
  	if (my_wq) {
@@ -129,6 +154,9 @@ static void mexit(void)
 {
 	flush_workqueue( my_wq );
 	destroy_workqueue( my_wq );
+
+	if (slab_chunk1)
+        kmem_cache_destroy(slab_chunk1);
 
 	printk("Exit %s.\n", THIS_MODULE->name);
 }
