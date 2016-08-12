55789599a1eddf1976c30564346e789da0515a8b
lxlenovostar <lxlenovostar@gmail.com>
2016-08-12 10:24:32 +0800

    练习

--- test/fls/test_fls.c
+++ test/fls/test_fls.c
@@ -6,13 +6,52 @@
 #include <linux/scatterlist.h>
 #include <linux/mm.h>
 
+/* Highest bit set in a byte */
+const char bytemsb[0x100] = { 
+  0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5,
+  5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7,
+  7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+  7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+  7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,                                                                            
+};
+
+/* Find last set (most significant bit) */
+static inline u_int fls32 (u_int32_t v)
+{
+  if (v & 0xffff0000) {
+    if (v & 0xff000000)
+      return 24 + bytemsb[v>>24];
+    else
+      return 16 + bytemsb[v>>16];
+  }
+  if (v & 0x0000ff00)
+    return 8 + bytemsb[v>>8];
+  else
+    return bytemsb[v];
+}
+
 static int minit(void)
 {
+    /*
     printk(KERN_INFO"result: %d", fls64(8));
     printk(KERN_INFO"result: %d", fls64(256));
     printk(KERN_INFO"result: %d", fls64(0));
     printk(KERN_INFO"result: %d", fls64(1));
+    */
+   
+    u_int64_t fingerprint_mask; // to check if we are at segment boundary  
+    unsigned int avg_segment_size = 32;   
+    
+    fingerprint_mask = (1 << (fls32(avg_segment_size)-1))-1; 
 
+    printk(KERN_INFO"fls32(avg_segment_size) is:%d, result: %llu", fls32(avg_segment_size), fingerprint_mask);
+    
     return 0;
 }
 
