f94355ed80f8f8628333069ed952f42625e2bf98
root <root@localhost.localdomain>
2016-10-08 02:38:10 -0700

    日常备份

--- model/build_receive_tcp/build_tcp/main.c
+++ model/build_receive_tcp/build_tcp/main.c
@@ -4,12 +4,22 @@
 
 #define MD5_LEN 16
 
-/*
-	build tcp header.
- */
+/* build tcp header. */
 int build_tcp(struct sk_buff *skb)
 {
+	
+
 	return 0;	
+}
+
+int copy_md5sum(struct sk_buff *skb)
+{
+	uint8_t md5_result[MD5LEN] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+	if (skb_tail_rome(skb) < MD5_LEN)
+		return 2;
+
+	return 0;
 }
 
 static int minit(void)
@@ -20,6 +30,8 @@ static int minit(void)
 	skb = alloc_skb(sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr) + MD5_LEN, GFP_ATOMIC);
 	if (skb == NULL)
 		return 1;
+
+	err = copy_md5(skb);
 
 	err = build_tcp(skb);
 
