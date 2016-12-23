4a8cfdb9efe8409d00b5418ff23f8599cf7071e1
root <root@localhost.localdomain>
2016-12-23 00:24:47 -0800

    内核模块修改完成

--- model/tcp_netlink/receive/kernel/lx_kernel.c
+++ model/tcp_netlink/receive/kernel/lx_kernel.c
@@ -1,14 +1,14 @@
 #include <linux/module.h>
-#include <net/sock.h>
-#include <linux/netlink.h>
 #include <linux/skbuff.h>
 #include <linux/timer.h>
-
-#define NETLINK_USER 31
+#include <net/sock.h>
+#include <linux/netlink.h>
 
 struct sock *nl_sk = NULL;
 int pid = 1;	
 struct timer_list message_timer;
+#define MY_MSG_TYPE (0x10 + 2)  // + 2 is arbitrary. same value for kern/usr
+DEFINE_MUTEX(my_mutex);
 
 void send_message(unsigned long data)
 {
@@ -52,38 +52,58 @@ void init_message_timer(void)
 	add_timer(&message_timer);
 }
 
-static void nl_data_ready(struct sk_buff *skb)
+static int 
+nl_data_ready(struct sk_buff *skb, struct nlmsghdr *r_nlh)
 {
-	struct nlmsghdr *nlh;
+	struct nlmsghdr *s_nlh;
+    int pid;
     struct sk_buff *skb_out;
     int msg_size;
     char *msg = "get your pid";
     int res;
+	int type;
 
-    nlh = (struct nlmsghdr *)skb->data;
-    printk(KERN_INFO "Netlink get pid:%s", (char *)nlmsg_data(nlh));
+    type = r_nlh->nlmsg_type;
+    if (type != MY_MSG_TYPE) {
+        printk(KERN_ERR "%s: expect %#x got %#x", __func__, MY_MSG_TYPE, type);
+        return -EINVAL;
+    }
 
-    pid = nlh->nlmsg_pid; /* pid of sending process */
+    printk(KERN_INFO "Netlink received msg payload:%s", (char *)NLMSG_DATA(r_nlh));
+
+    pid = r_nlh->nlmsg_pid; /*pid of sending process */
 
     msg_size = strlen(msg);
     skb_out = nlmsg_new(msg_size, 0);
     if (!skb_out) {
         printk(KERN_ERR "Failed to allocate new skb");
-        return;
+        return -ENOMEM;
     }
 
-    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+    s_nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
 
     NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
-    strncpy(nlmsg_data(nlh), msg, msg_size);
+    strncpy(nlmsg_data(s_nlh), msg, msg_size);
 
     res = nlmsg_unicast(nl_sk, skb_out, pid);
-    if (res < 0)
+    if (res < 0) {
         printk(KERN_INFO "Error while sending bak to user"); 
+		return res;
+	}
+
+	return 0;
+}
+
+static void
+nl_rcv_msg(struct sk_buff *skb)
+{
+    mutex_lock(&my_mutex);
+    netlink_rcv_skb(skb, &nl_data_ready);
+    mutex_unlock(&my_mutex);
 }
 
 static int netlink_init(void) {
-	nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, 0, nl_data_ready, NULL, THIS_MODULE);
+	nl_sk = netlink_kernel_create(&init_net, NETLINK_USERSOCK, 0, nl_rcv_msg, NULL, THIS_MODULE);
     if (!nl_sk) {
         printk(KERN_ALERT "Error creating socket.");
         return -1;
@@ -91,7 +111,6 @@ static int netlink_init(void) {
 
 	return 0;
 }
-
 
 static int __init my_module_init(void)
 {
