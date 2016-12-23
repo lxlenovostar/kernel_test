f0362b0fcbf53f0fcd0d23735fe6971340b1e5e0
root <root@localhost.localdomain>
2016-12-23 01:23:50 -0800

    修改部分代码

--- test/userspace/network/libevent/echo_server.c
+++ test/userspace/network/libevent/echo_server.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include <event2/event.h>
 #include <event2/bufferevent.h>
 
-#define LISTEN_PORT 9999
+#define LISTEN_PORT 9877
 #define LISTEN_BACKLOG 32
 
 void do_accept(evutil_socket_t listener, short event, void *arg);
@@ -16,7 +17,6 @@ void write_cb(struct bufferevent *bev, void *arg);
 
 int main(int argc, char *argv[])
 {
-    int ret;
     evutil_socket_t listener;
     listener = socket(AF_INET, SOCK_STREAM, 0);
     assert(listener > 0);
